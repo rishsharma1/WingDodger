ee866a73b72636ea9ff441052e50c4b9cdb4a669
Rishabh Sharma <rishsharma13@gmail.com>
2016-09-29 18:50:13 +1000

    camera controller

---
+++ WingDodger/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    /// <summary>
+    /// The game object that is controlled by the player
+    /// </summary>
+    public GameObject player;
+
+    /// <summary>
+    /// The difference between the player position and the camera position.
+    /// </summary>
+    private Vector3 offset;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        transform.position = player.transform.position + offset;
+	}
+}
