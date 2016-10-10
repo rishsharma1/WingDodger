7a860fed1e6a13916130ee1de0664a0e5bff36df
Rishabh Sharma <rishsharma13@gmail.com>
2016-10-11 00:50:10 +1100

    Plane movement using accelerometer

--- WingDodger/Assets/Scripts/Camera/CameraController.cs
+++ WingDodger/Assets/Scripts/Camera/CameraController.cs
@@ -20,7 +20,7 @@ public class CameraController : MonoBehaviour {
 
         offset = transform.position - player.transform.position;
 	}
-	
+	    
 	// Update is called once per frame
 	void LateUpdate () {
 
---
+++ WingDodger/Assets/Scripts/StealthBomber/BomberController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public  class BomberController : MonoBehaviour {
+
+    /// <summary>
+    /// The speed the plane traverses the terrain.
+    /// </summary>
+    public float planeVelocity;
+
+    /// <summary>
+    /// The speed the plane will roate at.
+    /// </summary>
+    public float rotationSpeed;
+
+    /// <summary>
+    /// The rigid body of the plane.
+    /// </summary>
+    public Rigidbody rigidBody;
+
+ 
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float currSpeed = Time.deltaTime * rotationSpeed;
+        this.transform.Translate(Input.acceleration.x * planeVelocity * Vector3.right * Time.deltaTime,Space.World);
+        transform.Rotate(Vector3.up, currSpeed * Input.acceleration.x);
+    } 
+}
