af1640c378a37fb9a514491f4ba09aff73eb20f3
Edge-PC\Edge <mingdongt@student.unimelb.edu.au>
2016-10-19 01:19:52 +1100

    exit_function

---
+++ WingDodger/Assets/Scripts/UI/exit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class exit : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        Application.Quit();
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+
+    }
+}
